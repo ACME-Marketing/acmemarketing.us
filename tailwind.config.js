f26c4f6661e9e3213b40f886a1c02bdc689b1bf0
saltlifeshep <62799544+saltlifeshep@users.noreply.github.com>
2025-07-02 14:58:29 -0400

    Switch to import syntax for @tailwindcss/typography in tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import typography from '@tailwindcss/typography';
+
+export default {
   content: [
     './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
   ],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [typography],
 };
 
